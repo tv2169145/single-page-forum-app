47529f254d5701cf5be3b988bb12f19fc290337f
zhean <tv2169145@gmail.com>
2018-10-06 20:13:21 +0800

    complete question crud

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -75,7 +75,8 @@ class QuestionController extends Controller
      */
     public function update(Request $request, Question $question)
     {
-        //
+        $question->update($request->all());
+        return response('update', Response::HTTP_ACCEPTED);
     }
 
     /**
