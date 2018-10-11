56b3d8c3762606e15849073e87cef725620902d3
Jimmy <tv2169145@gmail.com>
2018-10-12 02:22:46 +0800

    add jwt

--- config/auth.php
+++ config/auth.php
@@ -37,12 +37,12 @@ return [
 
     'guards' => [
         'web' => [
-            'driver' => 'session',
+            'driver' => 'api',
             'provider' => 'users',
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'jwt',
             'provider' => 'users',
         ],
     ],
